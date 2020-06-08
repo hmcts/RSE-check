c1bb906a759967da23562b860186af559453b629
David Gimelle <davidgimelle1@hmcts.net>
2020-06-08 16:46:55 +0100

    remove pmd

--- src/test/java/uk/gov/hmcts/reform/rsecheck/CaseDataUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/rsecheck/CaseDataUtilsTest.java
@@ -10,7 +10,7 @@ public class CaseDataUtilsTest {
 
     @Test
     public void givenDataWithCaseLink_thenReturnLinkValue() {
-        assertThat("Toto should be himself","Toto", is("Toto"));
+        assertThat("Toto should be himself", "Toto", is("Toto"));
     }
 
     @Test
@@ -21,16 +21,15 @@ public class CaseDataUtilsTest {
     @Test
     public void notAssertion() {
         String ranValuated = Double.toString(Math.random());
-        Boolean  valuatedRes = ranValuated.contains("1234");
+        Boolean valuatedRes = ranValuated.contains("1234");
         checkValuated(ranValuated);
     }
 
-    Boolean checkValuated(String valuated){
+    Boolean checkValuated(String valuated) {
         return null;
     }
 
-    boolean soManyParam(int p1,int p2, int p3, int p4, String p5, String p6, String p7, String p8, int p9){
+    boolean soManyParam(int p1, int p2, int p3, int p4, String p5, String p6, String p7, String p8, int p9) {
         return false;
     }
-
 }
