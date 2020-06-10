aa2f82297c6b0b9505b3cdd0c5073567ec3e31b1
David Gimelle <davidgimelle1@hmcts.net>
2020-06-10 15:05:04 +0100

    Add untested code

---
+++ src/main/java/uk/gov/hmcts/reform/rsecheck/UseLessStuff.java
@@ -0,0 +1,12 @@
+package uk.gov.hmcts.reform.rsecheck;
+
+public class UseLessStuff {
+
+
+    static private String NAME= "For testing";
+
+    public String addName(String prefix){
+        return prefix.concat(NAME);
+    }
+
+}
