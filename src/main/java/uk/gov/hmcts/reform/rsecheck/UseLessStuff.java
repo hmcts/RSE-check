2b1803b21dae2a21554db4cbbf511ae650530a90
David Gimelle <davidgimelle1@hmcts.net>
2020-06-10 15:52:24 +0100

    clean checkstyle

--- src/main/java/uk/gov/hmcts/reform/rsecheck/UseLessStuff.java
+++ src/main/java/uk/gov/hmcts/reform/rsecheck/UseLessStuff.java
@@ -2,18 +2,17 @@ package uk.gov.hmcts.reform.rsecheck;
 
 public class UseLessStuff {
 
+    static String aNAME = "For testing";
 
-    static private String NAME= "For testing";
-
-    public String addName(String prefix){
-        return prefix.concat(NAME);
+    public String addName(String prefix) {
+        return prefix.concat(aNAME);
     }
 
-    public Boolean checkName(String prefix){
+    public Boolean checkName(String prefix) {
         return prefix.contains(prefix);
     }
 
-    public String noName(String prefix){
+    public String noName(String prefix) {
         return "NoName";
     }
 
